243d72bd19a89524a84063facbe22dcd9b66ae57
Huarcaya98 <147445610+Huarcaya98@users.noreply.github.com>
2024-09-23 17:30:27 -0500

    Created Class Item

---
+++ Item.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen01_Taller3D
+{
+    public class Item
+    {
+        public string nombre;
+        public int valor;
+
+        protected Item(string nombre, int valor)
+        {
+            this.nombre = nombre;
+            this.valor = valor;
+        }
+      
+
+        
+    }
+}
