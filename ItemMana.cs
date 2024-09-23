a951b2f4632336090ae594a4941bdbac64d84bb8
Huarcaya98 <147445610+Huarcaya98@users.noreply.github.com>
2024-09-23 17:36:04 -0500

    Created Class ItemMana

---
+++ ItemMana.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen01_Taller3D
+{
+    public class ItemMana : Item
+    {
+
+        public ItemMana(string nombre, int valor)
+            : base(nombre , valor) { }
+
+    }
+}
