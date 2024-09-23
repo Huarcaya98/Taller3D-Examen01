c510c772559169b097425b02eee3d255dd026a28
Huarcaya98 <147445610+Huarcaya98@users.noreply.github.com>
2024-09-23 17:33:56 -0500

    Created Class ItemVida

---
+++ ItemVida.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen01_Taller3D
+{
+    public class ItemVida : Item
+    {
+
+        public ItemVida(string nombre, int valor )
+            :base(nombre, valor) { }
+
+    }
+}
