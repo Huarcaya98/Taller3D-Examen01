9c25012fdac0010ffddee494f607a9775a7ad572
Huarcaya98 <147445610+Huarcaya98@users.noreply.github.com>
2024-09-23 17:16:20 -0500

    Created Class Jugador and Poder
    -primero cree la clases Jugadorpero para crear sus metodos que dependia de poder asi  cree la clases Poder

---
+++ Jugador.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen01_Taller3D
+{
+    public  class Jugador : Personaje
+    {
+        public int mana;
+        public List<Poder> poderes = new List<Poder>();
+
+        public Jugador(string nombre, int vida, int danio, int mana)
+            :base(nombre, vida , danio)
+        {
+            this.mana = mana;
+        }
+
+        public override void Atacar(Personaje objetivo)
+        {
+
+            Console.WriteLine($"{nombre} , selecciona un ataque: 1. Ataque simple, 2. Usar poder");
+            int eleccion = int.Parse(Console.ReadLine());
+
+            if(eleccion == 2 && poderes.Count>0)
+            {
+
+                Console.WriteLine("Selecciona un poder");
+                for(int i = 0; i < poderes.Count; i++)
+                {
+                    Console.WriteLine($"{i + 1}.{poderes[i].nombre} - costo: {poderes[i].costoMana} Mana");
+                }
+
+                int eleccionPoder = int.Parse(Console.ReadLine()) - 1;
+                if(eleccionPoder >= 0 && eleccionPoder < poderes.Count)
+                {
+                    poderes[eleccionPoder].Usar(this, objetivo);
+                    return;
+                }
+
+            }base.Atacar(objetivo);
+
+        }
+
+    }
+}
---
+++ Poder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen01_Taller3D
+{
+    public class Poder
+    {
+        public string nombre;
+        public int danio;
+        public int costoMana;
+
+        public Poder(string nombre, int danio, int costoMana)
+        {
+            this.nombre = nombre;
+            this.danio = danio;
+            this.costoMana = costoMana;
+        }
+
+        public void Usar(Jugador jugador, Personaje objetivo)
+        {
+            if(jugador.mana >= costoMana)
+            {
+                jugador.mana -= costoMana;
+                objetivo.vida -= danio;
+                Console.WriteLine($"{jugador.nombre} usó {nombre} en {objetivo.nombre} y causó {danio} de daño");
+
+                if(objetivo.vida <=0)
+                {
+                    Console.WriteLine($"{objetivo.nombre} ha muerto");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"{jugador.nombre} no tiene sufuciente mana para usar {nombre}");
+            }
+        }
+        
+
+    }
+}
